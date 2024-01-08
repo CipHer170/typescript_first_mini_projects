e38d17f104b174a9f8722bee801bef6c52082948
CipHer170 <105492413+CipHer170@users.noreply.github.com>
2024-01-08 15:31:21 +0500

    added comments

--- src/context/contextData.tsx
+++ src/context/contextData.tsx
@@ -1,14 +1,17 @@
 import { createContext, useState } from "react";
 
+// adding context actions
 interface IModalContext {
   modal: boolean;
   open: () => void;
   close: () => void;
 }
-
+// creating context, with обозначениями каждого действия,
 export const ContextData = createContext<IModalContext>({
   modal: false,
+  // то что снизу обозначает, ()=setModal(true), поэтому они обозначаются функциями
   open: () => {},
+  // ()=setModal(false)
   close: () => {},
 });
 
